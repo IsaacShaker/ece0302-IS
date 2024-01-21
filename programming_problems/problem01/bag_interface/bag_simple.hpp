e5596b1674da40281a65e6e25fca565223be26fa
IsaacShaker <iss73@pitt.edu>
2024-01-21 14:42:29 -0500

    changed to inherit from abstract bag interface

--- programming_problems/problem01/bag_interface/bag_simple.hpp
+++ programming_problems/problem01/bag_interface/bag_simple.hpp
@@ -3,34 +3,34 @@
 
 #include "abstract_bag.hpp"
 
-template<typename T> class Bag{
-public:
+template <typename T>
+class Bag : public AbstractBag<T> {
+   public:
+    Bag();
 
-  Bag();
-  
-  ~Bag();
+    ~Bag();
 
-  std::size_t getCurrentSize() const;
+    std::size_t getCurrentSize() const;
 
-  bool isEmpty() const;
+    bool isEmpty() const;
 
-  bool add(const T& entry);
+    bool add(const T& entry);
 
-  bool remove(const T& entry);
+    bool remove(const T& entry);
 
-  void clear();
+    void clear();
 
-  std::size_t getFrequencyOf(const T& entry) const;
+    std::size_t getFrequencyOf(const T& entry) const;
 
-  bool contains(const T& entry) const;
+    bool contains(const T& entry) const;
 
-  static const std::size_t MAXSIZE = 100;
-  
-private:
-  // implementation using fixed automatic storage
-  std::size_t size;
-  
-  T data[MAXSIZE];
+    static const std::size_t MAXSIZE = 100;
+
+   private:
+    // implementation using fixed automatic storage
+    std::size_t size;
+
+    T data[MAXSIZE];
 };
 
 #include "bag_simple.tpp"
