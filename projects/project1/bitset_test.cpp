564d102061df25c4f2bcc1795304e00e1dbf8267
IsaacShaker <iss73@pitt.edu>
2024-01-22 11:59:48 -0500

    added 5 test cases

--- projects/project1/bitset_test.cpp
+++ projects/project1/bitset_test.cpp
@@ -1,30 +1,36 @@
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_COLOUR_NONE
-#include "catch.hpp"
-
-#include <iostream>
 #include "bitset.hpp"
 
-TEST_CASE( "Test bitset default construction", "[bitset]" ) {
+#include <iostream>
 
-    Bitset b;  
+#include "catch.hpp"
+
+TEST_CASE("Test bitset default construction", "[bitset]") {
+    Bitset b;
     REQUIRE(b.size() == 8);
     REQUIRE(b.good());
 }
 
-TEST_CASE( "Test bitset construction with asString", "[bitset]" ) {
+TEST_CASE("Test bitset construction with asString", "[bitset]") {
     Bitset b;
     REQUIRE(b.asString().compare("00000000") == 0);
 }
 
-TEST_CASE( "Test bitset construction size", "[bitset]" ) {
+TEST_CASE("Test bitset construction size", "[bitset]") {
     Bitset b(64);
     std::string s(64, '0');
     REQUIRE(b.size() == 64);
     REQUIRE(b.good());
 }
 
-TEST_CASE( "Test bitset construction string", "[bitset]" ) {
+TEST_CASE("Test bitset construction invalid size", "[bitset]") {
+    Bitset b1(0), b2(-2);
+    REQUIRE_FALSE(b1.good());
+    REQUIRE_FALSE(b2.good());
+}
+
+TEST_CASE("Test bitset construction string", "[bitset]") {
     std::string s("00101110000011000001101000001");
     Bitset b(s);
     REQUIRE(b.size() == s.size());
@@ -32,13 +38,19 @@ TEST_CASE( "Test bitset construction string", "[bitset]" ) {
     REQUIRE(b.asString().compare(s) == 0);
 }
 
-TEST_CASE( "Test bitset construction invalid string", "[bitset]" ) {
+TEST_CASE("Test bitset construction invalid string", "[bitset]") {
     std::string s("00101110000011002001101000001");
     Bitset b(s);
     REQUIRE_FALSE(b.good());
 }
 
-TEST_CASE( "Test set", "[bitset]" ) {
+TEST_CASE("Test bitset construction empty string", "[bitset]") {
+    std::string s("");
+    Bitset b(s);
+    REQUIRE_FALSE(b.good());
+}
+
+TEST_CASE("Test set", "[bitset]") {
     std::string s("00010001");
     Bitset b;
     b.set(3);
@@ -48,7 +60,7 @@ TEST_CASE( "Test set", "[bitset]" ) {
     REQUIRE(b.asString().compare(s) == 0);
 }
 
-TEST_CASE( "Test set with size init", "[bitset]" ) {
+TEST_CASE("Test set with size init", "[bitset]") {
     std::string s("00010");
     Bitset b(5);
     b.set(3);
@@ -57,20 +69,47 @@ TEST_CASE( "Test set with size init", "[bitset]" ) {
     REQUIRE(b.asString().compare(s) == 0);
 }
 
-TEST_CASE( "Test combined", "[bitset]" ) {
-    std::string s((1<<15) + 3, '0');
+TEST_CASE("Test reset with construction string", "[bitset]") {
+    std::string s("11111");
+    Bitset b(s);
+    b.reset(0);
+    b.reset(3);
+    REQUIRE(b.good());
+    REQUIRE(b.asString().compare("01101") == 0);
+}
+
+TEST_CASE("Test toggle with construction string", "[bitset]") {
+    std::string s("00100");
+    Bitset b(s);
+    b.toggle(0);
+    b.toggle(2);
+    REQUIRE(b.good());
+    REQUIRE(b.asString().compare("10000") == 0);
+}
+
+TEST_CASE("Test test method with construction string", "[bitset]") {
+    std::string s("00100");
+    Bitset b(s);
+    REQUIRE(b.size() == 5);
+    REQUIRE(b.good());
+    REQUIRE(b.test(0) == 0);
+    REQUIRE(b.test(2) == 1);
+}
+
+TEST_CASE("Test combined", "[bitset]") {
+    std::string s((1 << 15) + 3, '0');
     Bitset b(s);
     REQUIRE(b.size() == s.size());
     REQUIRE(b.good());
     REQUIRE(b.asString().compare(s) == 0);
 
-    for (int i = 1; i < (1<<10); i++) {
+    for (int i = 1; i < (1 << 10); i++) {
         b.set(i);
-        b.reset(i + (1<<10));
-        b.toggle(i + (1<<11));
+        b.reset(i + (1 << 10));
+        b.toggle(i + (1 << 11));
         REQUIRE(b.good());
         REQUIRE(b.test(i));
-        REQUIRE_FALSE(b.test(i + (1<<10)));
-        REQUIRE(((b.test(i + (1<<11)) == true && s.at(i + (1<<11)) == '0') || (b.test(i + (1<<11)) == false && s.at(i + (1<<11)) == '1')));
+        REQUIRE_FALSE(b.test(i + (1 << 10)));
+        REQUIRE(((b.test(i + (1 << 11)) == true && s.at(i + (1 << 11)) == '0') || (b.test(i + (1 << 11)) == false && s.at(i + (1 << 11)) == '1')));
     }
 }
