35019da47a4bee3ce3b129a32b928fc038fc479f
IsaacShaker <iss73@pitt.edu>
2024-02-17 12:01:50 -0500

    Implemented convert function (postfix to prefix)

--- programming_problems/problem04/postpre/algebraic_expressions.cpp
+++ programming_problems/problem04/postpre/algebraic_expressions.cpp
@@ -1,4 +1,6 @@
+#include <stack>
 #include <string>
+using std::stack;
 using std::string;
 
 #include <cctype>  // for isalpha
@@ -41,5 +43,37 @@ bool isPost(string s) {
 }
 
 void convert(string &postfix, string &prefix) {
-    // TODO
+    // check if valid post before attempting to convert
+    if (isPost(postfix) == false) {
+        return;
+    }
+
+    // perform conversion
+    // read postfix from left to right
+    stack<string> post_to_pre;
+    string arith_operator, operand1, operand2, temp;
+
+    for (int i = 0; i < postfix.size(); i++) {
+        // check if character is an operator
+        if (isoperator(postfix[i])) {
+            arith_operator = postfix[i];
+            operand1 = post_to_pre.top();
+            post_to_pre.pop();
+            operand2 = post_to_pre.top();
+            post_to_pre.pop();
+
+            post_to_pre.push(arith_operator + operand2 + operand1);
+        } else {
+            // convet the character to string to avoid errors
+            temp = postfix[i];
+            post_to_pre.push(temp);
+        }
+    }
+
+    // concatonate all strings in the stack to make final prefix form
+    postfix = "";
+    while (post_to_pre.empty() == false) {
+        postfix += post_to_pre.top();
+        post_to_pre.pop();
+    }
 }
